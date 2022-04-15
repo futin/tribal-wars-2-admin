e5a88bb1c67dccc49c9b16eb5f6648fd3160fdbb
ajevtic <andreja.jevtic@timify.com>
2022-04-15 08:50:39 +0200

    started emitter refactor

--- webstorm.js
+++ webstorm.js
@@ -3,6 +3,7 @@ System.config({
   paths: {
     "@global-constants": "./lib/constants",
     "@global-utils": "./lib/utils",
-    "@tw-executors": "./lib/tw"
+    "@tw-executors": "./lib/tw",
+    "@custom-emitter": "./lib/custom-emitter"
   }
 })
