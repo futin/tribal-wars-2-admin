74609b234659fc1e611999b6ec0e4f8264acd16d
ajevtic <andreja.jevtic@timify.com>
2022-04-22 17:37:44 +0200

    minor refactoring

--- webstorm.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-System.config({
-  paths: {
-    "#global-utils": "./lib/utils.js",
-    "#global-constants": "./lib/constants.js",
-    "#tw-executors": "./lib/tw/index.js",
-    "#custom-emitter": "./lib/custom-emitter.js"
-  }
-})
